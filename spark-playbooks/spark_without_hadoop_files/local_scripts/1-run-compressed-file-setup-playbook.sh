1a3d947f18f58024a608fc1412198af94a330b5c
brian-sigurdson <bsigurd@bgsu.edu>
2017-12-16 14:07:19 -0500

    spark (without hadoop files) compressed file setup implemented

--- spark-playbooks/spark_without_hadoop_files/local_scripts/1-run-compressed-file-setup-playbook.sh
+++ spark-playbooks/spark_without_hadoop_files/local_scripts/1-run-compressed-file-setup-playbook.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Purpose
-# bash script to initiate the Ansible playbook to deploy Hadoop
-# run as:  ./run-hadoop-1-deploy-playbook.sh 1.2.1
+# bash script to initiate the Ansible playbook to deploy Spark
+# run as:  ./run-compressed-file-steup-playbook.sh 1.2.1
 
 
 #########################################################################################################
@@ -13,9 +13,8 @@ func_print_usage(){
   echo "eg. version 1.2.1 should be run as: ./<script-name>.sh 1.2.1"
 }
 
-# hadoop_ver parameter should be in the form a.b.c
-# this will be used to create the installation directory, such as /opt/hadoop/a.b.c
-# and a hadoop user for this specific version, such as hadoop_abc
+# spark_ver parameter should be in the form a.b.c
+# this will be used to create the installation directory, such as /opt/spark/a.b.c
 
 # just basic sanity test to catch likely errors by say a tired user
 # test for null
@@ -46,28 +45,25 @@ c=$(echo -n $1 | cut -c5)
 # vars
 #########################################################################################################
 
-hadoop_ver="$a.$b.$c"
-hadoop_ver_numeric="$a$b$c"
-extra_vars="hadoop_version=$hadoop_ver hadoop_version_numeric=$hadoop_ver_numeric"
+spark_ver="$a.$b.$c"
+spark_ver_numeric="$a$b$c"
+extra_vars="spark_version=$spark_ver"
 
 #########################################################################################################
 # code 
 #########################################################################################################
 # test for numeric value
 re='^[0-9]+$'
-if ! [[ $hadoop_ver_numeric =~ $re ]] ; then
+if ! [[ $spark_ver_numeric =~ $re ]] ; then
    	echo "error: Not a number" >&2; 
 	func_print_usage
 	exit 1
 fi
 
-if [[ hadoop_ver_numeric -ge 220 && hadoop_ver_numeric -le 274 ]]; then
-	# version 2.2.0 - 2.7.4
-	ansible-playbook ../local_playbooks/compressed-file-setup_220_274.yml -e "$extra_vars"
-elif [[ hadoop_ver_numeric -gt 274 && hadoop_ver_numeric -le 300 ]]; then
-	# version 2.8.0 - 2.9.0
-	ansible-playbook ../local_playbooks/compressed-file-setup_280_300.yml -e "$extra_vars"
+if [[ spark_ver_numeric -ge 200 && spark_ver_numeric -le 221 ]]; then
+	# version 2.0.0 - 2.2.1
+	ansible-playbook ../local_playbooks/compressed-file-setup.yml -e "$extra_vars"
 else
-	echo "Version number is not between 2.2.0 and 3.0.0"
+	echo "Version number is not between 2.0.0 and 2.2.1"
 fi
  
