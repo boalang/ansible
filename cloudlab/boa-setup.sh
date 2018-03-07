a645e0f94f00123e59e3eeeb1600ddc2b260eb56
brian-sigurdson <sigurdson.brian@gmail.com>
2018-03-07 06:23:41 -0700

    updates

--- cloudlab/boa-setup.sh
+++ cloudlab/boa-setup.sh
@@ -9,8 +9,12 @@ PATH_TO_BOA_DIR=$PATH_TO_ANSIBLE_DIR/boa
 # create link to ansible.cfg file
 ln -s $PATH_TO_ANSIBLE_DIR/ansible.cfg $PATH_TO_BOA_DIR/ansible.cfg
 
-echo "MASTER_NAME=$MASTER_NAME" >> /tmp/boa1.log
-echo "PATH_TO_BOA_DIR=$PATH_TO_BOA_DIR" >> /tmp/boa1.log
+#echo "MASTER_NAME=$MASTER_NAME" >> /tmp/boa1.log
+#echo "PATH_TO_BOA_DIR=$PATH_TO_BOA_DIR" >> /tmp/boa1.log
 
-ansible-playbook $PATH_TO_BOA_DIR/setup-boa.yml --extra-vars "master_name=$MASTER_NAME" | tee -a /tmp/boa2.log
+#cat /home/ansible/ansible/local_hosts/hosts >> /tmp/boa3.log
+#cat /home/ansible/ansible/ansible.cfg >> /tmp/boa4.log
+
+cd $PATH_TO_BOA_DIR
+ansible-playbook $PATH_TO_BOA_DIR/setup-boa.yml | tee -a /tmp/boa.log
 
